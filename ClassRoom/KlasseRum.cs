f57b76445610e4399afb004b92dd6fa90003718b
Sami Rherbi <sami0771@edu.zealand.dk>
2022-09-05 13:33:27 +0200

    prop, d-ctor

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class KlasseRum
+    {
+        private string _klasserum;
+        private List<Studerende> _klasseliste;
+        private DateTime _semesterstart;
+
+        public string Klasserum
+        {
+            get { return _klasserum; }
+            set { _klasserum = value; }
+        }
+
+        public List<Studerende> Klasseliste
+        {
+            get { return _klasseliste; }
+            set { _klasseliste = value; }
+        }
+
+        public DateTime Semsterstart
+        {
+            get { return _semesterstart; }
+            set { _semesterstart = value; }
+        }
+        public KlasseRum()
+        {
+
+        }
+    }
+}
